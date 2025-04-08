6f18d4c1e278f7ed2730fef2c4432372f177acc4
cbj193 <cbj193@msstate.edu>
2025-04-08 13:11:48 -0500

    Update plant_tender_main.ino

--- plant_tender_main/plant_tender_main.ino
+++ plant_tender_main/plant_tender_main.ino
@@ -12,16 +12,22 @@ const int waterLevelSensorPin = A0;
 const int lowWaterVal = 10;
 bool tankLow = false;
 
+// Light Detection and Emission
+const int lightSensor = A1; 
+const int relaySwitchPin = 7;
+
 void setup() {
   Serial.begin(115200);
 
   // confirm the connection
   if (!ss.begin(0x36)) {
-    Serial.println("ERROR! Soil Sensor not found");
+	Serial.println("ERROR! Soil Sensor not found");
     while(1) delay(1);
   } else {
     Serial.print("Soil Sensor started!");
   }
+	
+	pinMode(relaySwitchPin, OUTPUT); // set relaySwitchPin for light emission to output
 }
 
 void loop() {
@@ -59,6 +65,27 @@ void loop() {
   }else{
     analogWrite(pumpPin, 0);
   }
+
+	/*
+	Light detection and Emission
+	*/
+  int raw_light = analogRead(lightSensor); // read the raw value from lightSensor pin (A1)
+  int light = map(raw_light, 0, 1023, 0, 100); // map the value from 0, 1023 to 0, 100
+  
+  // if conditions are right for light to turn on
+  if (light <= 40) 
+  {
+    digitalWrite(relaySwitchPin, HIGH);
+  }
+
+  // else, turn lights on
+  else
+  {
+	  digitalWrite(relaySwitchPin, LOW);
+  }
+ 
+  Serial.print("Light level: "); 
+  Serial.println(light); // print the light value in Serial Monitor
 
   delay(100);
 }
